bd94696c192052d7dafc8446b184069469fda3fd
Artur M. Wolff <artur.m.wolff@gmail.com>
2019-10-29 21:50:28 +0100

    Make ShortestPath faster

--- path.go
+++ path.go
@@ -1,5 +1,53 @@
 package graph
 
+// OptimizedShortestPath computes a shortest path from v to w.
+// Only edges with non-negative costs are included.
+// The number dist is the length of the path, or -1 if w cannot be reached.
+//
+// The pessimistic time complexity is O((|E| + |V|)⋅log|V|), where |E| is the
+// number of edges and |V| the number of vertices in the graph.
+func OptimizedShortestPath(g Iterator, v, w int) (path []int, dist int64) {
+	n := g.Order()
+	dists := make([]int64, n)
+	parents := make([]int, n)
+	for i := range dists {
+		dists[i], parents[i] = -1, -1
+	}
+	dists[v] = 0
+
+	// Dijkstra's algorithm
+	q := emptyPrioQueue(dists)
+	q.Push(v)
+	for q.Len() > 0 {
+		u := q.Pop()
+		if u == w {
+			for x := w; x != -1; x = parents[x] {
+				path = append(path, x)
+			}
+			for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+				path[i], path[j] = path[j], path[i]
+			}
+			return path, dists[w]
+		}
+		g.Visit(u, func(x int, d int64) (skip bool) {
+			if d < 0 {
+				return
+			}
+			alt := dists[u] + d
+			switch {
+			case dists[x] == -1:
+				dists[x], parents[x] = alt, u
+				q.Push(x)
+			case alt < dists[x]:
+				dists[x], parents[x] = alt, u
+				q.Fix(x)
+			}
+			return
+		})
+	}
+	return []int{}, -1
+}
+
 // ShortestPath computes a shortest path from v to w.
 // Only edges with non-negative costs are included.
 // The number dist is the length of the path, or -1 if w cannot be reached.
